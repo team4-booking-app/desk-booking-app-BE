f4c2966e4eb1a20e94ac865821631d31848d0b92
Mantas <mantas.ambrazaitis9@gmail.com>
2022-04-05 12:45:41 +0300

    v0.0

---
+++ src/main/java/com/team4/deskbookingappbe/DeskBookingAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.team4.deskbookingappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskBookingAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskBookingAppBeApplication.class, args);
+    }
+
+}
