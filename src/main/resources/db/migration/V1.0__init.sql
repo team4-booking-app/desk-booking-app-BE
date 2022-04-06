5615c487ddb5f1eca38fc9d43b2b51365dfe9552
KarolisKriauciunas <karoliomeilas@gmail.com>
2022-04-06 15:37:53 +0300

    Commit: Working POST on local database

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -3,12 +3,7 @@
 CREATE TABLE user (
                       USERID bigint PRIMARY KEY AUTO_INCREMENT,
                         first_name varchar(255) not null,
-                        middle_name varchar(255),
                         last_name varchar(255) not null,
                         email varchar(255) not null,
-                        phone varchar(255),
                         password varchar(255) not null
 );
---Person data
-insert into user (USERID, first_name, middle_name, last_name, email, phone, password) values (38202020577,'Testas', '',  'Testauskas', 'test@test.com', '+37060035645','haha' );
-insert into user (USERID, first_name, middle_name, last_name, email, phone, password) values (38202020567,'Testas1', '',  'Testauskas1', 'test@test.com', '+37060035666','labas' );
