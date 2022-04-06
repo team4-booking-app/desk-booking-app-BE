94a401807eb344b001998c47a7c4739865e2057a
KarolisKriauciunas <karoliomeilas@gmail.com>
2022-04-06 11:28:08 +0300

    Trying to create a User object for data

---
+++ src/main/java/model/domain/User.java
@@ -0,0 +1,34 @@
+package model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigInteger;
+
+@Data
+@Entity(name = "USER")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @Column(name = "USERID", nullable = false)
+    private Long userID;
+
+    @Column(name = "FIRST_NAME", nullable = false)
+    private  String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false)
+    private  String lastName;
+
+    @Column(name = "EMAIL")
+    private  String email;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+}
--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -1,13 +1,14 @@
 --Swedbank demo database
 --Person table
-CREATE TABLE person (
-                        pid bigint PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE user (
+                      USERID bigint PRIMARY KEY AUTO_INCREMENT,
                         first_name varchar(255) not null,
                         middle_name varchar(255),
                         last_name varchar(255) not null,
-                        email varchar(255),
-                        phone varchar(255)
+                        email varchar(255) not null,
+                        phone varchar(255),
+                        password varchar(255) not null
 );
 --Person data
-insert into person (pid, first_name, middle_name, last_name, email, phone) values (38202020577,'Testas', '',  'Testauskas', 'test@test.com', '+37060035645' );
-insert into person (pid, first_name, middle_name, last_name, email, phone) values (38202020567,'Testas1', '',  'Testauskas1', 'test@test.com', '+37060035666' );
+insert into user (USERID, first_name, middle_name, last_name, email, phone, password) values (38202020577,'Testas', '',  'Testauskas', 'test@test.com', '+37060035645','haha' );
+insert into user (USERID, first_name, middle_name, last_name, email, phone, password) values (38202020567,'Testas1', '',  'Testauskas1', 'test@test.com', '+37060035666','labas' );
