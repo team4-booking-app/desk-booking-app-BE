bcf923b7632f93abe62ed13d7856e8e9287dda42
Justinas Va <vaskelis70@gmail.com>
2022-04-07 16:21:07 +0300

    Added RequestMapping

--- src/main/java/com/team4/deskbookingappbe/controller/UserController.java
+++ src/main/java/com/team4/deskbookingappbe/controller/UserController.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/v1/registration")
 public class UserController {
 
     private final UserService userService;
