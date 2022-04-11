40450a6b5682ab3e38d59518dd514855a2a6edaa
Mantas <mantas.ambrazaitis9@gmail.com>
2022-04-11 14:14:14 +0300

    Config tweaks

--- src/main/java/com/team4/deskbookingappbe/config/CorsConfig.java
+++ src/main/java/com/team4/deskbookingappbe/config/CorsConfig.java
@@ -3,9 +3,11 @@ package com.team4.deskbookingappbe.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig {
 
     @Bean
@@ -17,10 +19,10 @@ public class CorsConfig {
                         .allowedMethods("GET","POST","PUT","DELETE")
                         .allowedHeaders("*")
                         .allowedOrigins("http://team4-frontend-stage-app.herokuapp.com",
-                                        "http://team4-frontend-stage-app.herokuapp.com:80",
-                                        "https://team4-frontend-stage-app.herokuapp.com:80",
                                         "https://team4-frontend-stage-app.herokuapp.com",
-                                        "https://team4-frontend-stage-app.herokuapp.com/registration");
+                                        "https://team4-frontend-stage-app.herokuapp.com/registration",
+                                        "http://team4-frontend-stage-app.herokuapp.com/registration",
+                                        "*");
             }
         };
     }
