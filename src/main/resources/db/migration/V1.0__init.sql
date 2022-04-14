f34baa9fe9a50897b97ea5b8a5ac1a20d1e47c46
Justinas Va <vaskelis70@gmail.com>
2022-04-14 14:49:59 +0300

    x

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS Desk (
                        desk_id SERIAL UNIQUE,
                        room_name TEXT REFERENCES Rooms (room_name)
 );
---
---CREATE TABLE IF NOT EXISTS Reservations (
---                       reservation_id SERIAL,
---                       user_email TEXT REFERENCES Users (email),
---                       desk_number INT REFERENCES Desk (desk_id),
---                       reservation_start TIMESTAMP NOT NULL,
---                       reservation_end TIMESTAMP NOT NULL
---);
+
+CREATE TABLE IF NOT EXISTS Reservations (
+                       reservation_id SERIAL,
+                       user_email TEXT REFERENCES Users (email),
+                       desk_number INT REFERENCES Desk (desk_id),
+                       reservation_start TIMESTAMP NOT NULL,
+                       reservation_end TIMESTAMP NOT NULL
+);
 --
 ----======================================
 --
